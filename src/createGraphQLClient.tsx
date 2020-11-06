4fbb712fef6e9b35ea68e3328b45d1c372ae4f96
shambhand <33409682+shambhand@users.noreply.github.com>
2020-11-06 09:48:51 +0530

    Update createGraphQLClient.tsx

--- src/createGraphQLClient.tsx
+++ src/createGraphQLClient.tsx
@@ -3,7 +3,7 @@ import { ApolloClient, createNetworkInterface } from 'react-apollo';
 // 	http://dev.apollodata.com/react/initialization.html#creating-client
 export const createGraphQLClient = () => {
   const networkInterface = createNetworkInterface({
-    uri: 'http://localhost:9977/graphql'
+    uri: 'http://spring-petclinic-backend/graphql'
   });
   const client = new ApolloClient({
     networkInterface: networkInterface
